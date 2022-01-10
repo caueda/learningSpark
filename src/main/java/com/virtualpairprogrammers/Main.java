fc3d0d6196837cdb0197cc77d9db7a81dde39420
carlos.ueda <carlos.ueda@gmail.com>
2022-01-10 11:59:00 -0400

    Join example

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.avro.generic.GenericData;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
@@ -26,20 +27,24 @@ public class Main {
 				.setAppName("startingSpark")
 				.setMaster("local[*]");
 		JavaSparkContext sc = new JavaSparkContext(conf);
-		
-		JavaRDD<String> initialRdd = sc.textFile("src/main/resources/subtitles/input.txt");
 
-		initialRdd
-				.map(sentence -> sentence.replaceAll("[^a-zA-Z\\s]", "").toLowerCase())
-				.filter(sentence -> sentence.trim().length() > 0)
-				.flatMap(sentence -> Arrays.asList(sentence.split(" ")).iterator())
-				.filter(word -> word.trim().length() > 0)
-				.filter(Util::isNotBoring)
-				.mapToPair(word -> new Tuple2<String, Long>( word, 1L))
-				.reduceByKey((v1, v2) -> v1 + v2)
-				.mapToPair(stringLongTuple2 -> new Tuple2<>(stringLongTuple2._2, stringLongTuple2._1))
-				.sortByKey(false)
-				.take(10).forEach(System.out::println);
+		List<Tuple2<Integer, Integer>> visitsRaw = new ArrayList<>();
+		visitsRaw.add(new Tuple2<>(4, 19));
+		visitsRaw.add(new Tuple2<>(6, 4));
+		visitsRaw.add(new Tuple2<>(10, 9));
+
+		List<Tuple2<Integer, String>> usersRaw = new ArrayList<>();
+		usersRaw.add(new Tuple2<>(1, "John"));
+		usersRaw.add(new Tuple2<>(2, "Bob"));
+		usersRaw.add(new Tuple2<>(3, "Alan"));
+		usersRaw.add(new Tuple2<>(4, "Doris"));
+		usersRaw.add(new Tuple2<>(5, "Marybelle"));
+		usersRaw.add(new Tuple2<>(6, "Raquel"));
+
+		JavaPairRDD<Integer, Integer> visits = sc.parallelizePairs(visitsRaw);
+		JavaPairRDD<Integer, String> users = sc.parallelizePairs(usersRaw);
+
+		visits.join(users).foreach(value -> System.out.println(value));
 
 		sc.close();
 	}
