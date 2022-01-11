21a7ab6978d5803da80e237153e9f82bbd752c1c
carlos.ueda <carlos.ueda@gmail.com>
2022-01-11 15:38:52 -0400

    agg method in Spark

---
+++ src/main/java/com/virtualpairprogrammers/MainExamResults.java
@@ -0,0 +1,37 @@
+package com.virtualpairprogrammers;
+
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.max;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+
+public class MainExamResults {
+
+	public static void main(String[] args) {
+		System.setProperty("hadoop.home.dir", "c:/hadoop");
+		Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+		SparkSession sparkSession = SparkSession.builder()
+				.appName("testingSql").master("local[*]")
+				.config("spark.sql.warehouse.dir", "file:///c:/tmp/")
+				.getOrCreate();
+		Dataset<Row> dataset = sparkSession
+				.read()
+				.option("header", true)
+				.csv("src/main/resources/exams/students.csv");
+
+		dataset = dataset.groupBy("subject")
+				.agg(max(col("score").cast(DataTypes.IntegerType)).alias("max score "));
+
+		dataset.show(100);
+
+		sparkSession.close();
+	}
+
+}
